d44a6a00e778fd3fb9de8eb3d3e88092e548b377
caesuric <illusorycaesura@gmail.com>
2018-02-08 08:39:34 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+var path = require('path')
+const webpack = require('webpack')
+var WebpackBuildNotifierPlugin = require('webpack-build-notifier')
+
+modules.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'web'),
+        filename: 'js/index.js',
+        sourcePrefix: ''
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(css|scss)$/,
+                loaders: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                loaders: [
+                    'html-loader'
+                ]
+            },
+            {
+                test: /\.(eot|woff|woff2|svg|ttf|png|jpg)([\?]?.*)$/,
+                loaders: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ],
+        unknownContextCritical: false
+    },
+    devtool: 'source-map',
+    resolve: {
+        alias: {
+            jquery: 'jquery/src/jquery'
+        },
+        modules: [
+            'node_modules',
+            path.resolve(__dirname, 'src/lib'),
+            path.resolve(__dirname, 'src/scss')
+        ]
+    },
+    plugins: {
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new WebpackBuildNotifierPlugin({})
+    }
+}
