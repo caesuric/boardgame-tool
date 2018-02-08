bac0aaede85cfdd7c6707af2ffedd46b13553c87
caesuric <illusorycaesura@gmail.com>
2018-02-08 12:34:20 -0500

    added requirements.txt

--- server/datasocket.py
+++ server/datasocket.py
@@ -12,7 +12,7 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
             self.query_games(parsed)
     def query_games(self, message):
         games = [{'name': x.name, 'rank': x.rank, 'minPlayers': x.min_players, 'maxPlayers': x.max_players, 'minTime': x.min_time, 'maxTime': x.max_time, 'imageUrl': x.image_url} for x in data.DataHandler.data if x.min_players<int(message['players']) and x.max_players>int(message['players']) and x.max_time<=int( message['minutes'])]
-        self.attempt_to_write_message(unicode(tornado.escape.json_encode({'message': 'gameList', 'games': games}), errors='ignore'))
+        self.attempt_to_write_message({'message': 'gameList', 'games': games})
     def attempt_to_write_message(self, message):
         try:
             self.write_message(message)
